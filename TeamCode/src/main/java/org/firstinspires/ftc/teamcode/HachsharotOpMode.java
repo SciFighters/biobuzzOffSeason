7fcdeec5e674c6c0a45bdd46c9b72fa198f3cd56
Scifighters <ftctelmond@gmail.com>
2026-05-09 21:52:04 +0300

    dan bloomberg hachsharot file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HachsharotOpMode.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.decodeOpModes.testers;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.seattlesolvers.solverslib.command.CommandOpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+@TeleOp
+public class HachsharotOpMode extends CommandOpMode {
+    GamepadEx gamepad;
+    DcMotorEx FL;
+    DcMotorEx FR;
+    DcMotorEx BL;
+    DcMotorEx BR;
+}
