f3d71371fae19660ddb8f9ceee7ab768713bcba0
Ori Supino <ori@supino.org>
2026-09-14 20:51:28 +0300

    added Pedro pinpoint tuning

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -5,6 +5,7 @@ import com.pedropathing.tuning.autotune.Procedure;
 import com.pedropathing.tuning.autotune.Tuner;
 
 import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.PinpointTuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.Tests;
 
 public class Tuning {
@@ -12,6 +13,10 @@ public class Tuning {
     @Tuner
     public static Procedure mecanumTuner() {
         return new MecanumTuner();
+    }
+    @Tuner
+    public static Procedure pinpointTuner() {
+        return new PinpointTuner();
     }
     @Tuner
     public static Procedure tests() {
