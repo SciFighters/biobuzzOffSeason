de9d704c11372aa53bb7b955d47fd01a094d3f97
dordib <braundorel1@gmail.com>
2026-09-19 21:28:59 +0300

    tried to make path work

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-import com.pedropathing.api.Paths;
+import static com.pedropathing.api.Paths.curve;
+
+
 import com.pedropathing.api.PoseFactory;
 import com.pedropathing.follower.Follower;
 
@@ -16,21 +18,26 @@ import org.firstinspires.ftc.teamcode.pedro.Constants;
 public class TestAuto extends CommandOpMode {
     Follower follower;
     PoseFactory poseFactory = PoseFactory.degrees();
-    private final Pose start = poseFactory.of(70.4172, 8, 90);
-    private final Pose straight = poseFactory.of(70.4981, 8.2706, 90);
+    private final Pose start = poseFactory.of(70.4172, 8, 0);
+    private final Pose straight = poseFactory.of(70.4981, 8.2706, -90);
     private final Pose straightControl1 = poseFactory.of(130.0363, 68.5784, 0);
     private final Pose straightControl2 = poseFactory.of(65.6845, 115.8776, 0);
     private final Pose straightControl3 = poseFactory.of(69.0344, 109.6998, 0);
-    private final Pose straightSegment1Target = poseFactory.of(71.5, 71.5, 0);
-    private final Pose straightSegment2Heading = poseFactory.of(70.4981, 8.2706, 0);
+    private final Pose straightSegment1Target = poseFactory.of(72, 71.5, 0);
+    private final Pose straightSegment2Start = poseFactory.of(70.4981, 8.2706, -92.4403);
+    private final Pose straightSegment2End = poseFactory.of(70.4981, 8.2706, -90);
     @Override
     public void initialize() {
         super.reset();
         follower = Constants.create(hardwareMap);
         follower.setPose(start);
-        Path path = Paths.curve(start, straightControl1, straightControl2, straightControl3, straight).constant(straight);
-//                .heading(Interpolator.piecewise().until(0.5998, Interpolator.facingPoint(straightSegment1Target))
-//                        .until(1, Interpolator.constant(straightSegment2Heading)));
+        follower.update();
+        Path path =
+        curve(start, straightControl1, straightControl2, straightControl3, straight)
+                .heading(Interpolator.piecewise()
+                    .until(0.5998, Interpolator.facingPoint(straightSegment1Target))
+                    .until(1, Interpolator.linear(straightSegment2Start, straightSegment2End)));
+
 
         schedule(new FollowPathCommand(follower,path));
     }
