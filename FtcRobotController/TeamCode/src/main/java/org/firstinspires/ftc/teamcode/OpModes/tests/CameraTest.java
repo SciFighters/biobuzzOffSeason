a4cf25bd3808adbd2136602bd60e7e8af66e4d5b
Ori Supino <ori@supino.org>
2026-09-26 23:48:13 +0300

    look at the pollen but don't make it obvious

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tests/CameraTest.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/tests/CameraTest.java
@@ -1,8 +1,13 @@
 package org.firstinspires.ftc.teamcode.OpModes.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 
 import org.firstinspires.ftc.teamcode.Utilities.ActionOpMode;
+import org.firstinspires.ftc.teamcode.pedro.Constants;
 import org.firstinspires.ftc.teamcode.subSystems.IntakeCamera;
 import org.firstinspires.ftc.vision.opencv.ColorRange;
 import org.opencv.core.Point;
@@ -12,18 +17,50 @@ import java.util.List;
 @TeleOp(name = "CameraTester", group = "Test")
 public class CameraTest extends ActionOpMode {
     private IntakeCamera camera;
+    private Follower follower;
+    GamepadEx gamepad;
 
     @Override
     public void initialize() {
+        follower = Constants.create(hardwareMap);
+        follower.manual(0, 0, 0);
         camera = new IntakeCamera(hardwareMap);
         camera.scanBlobs(ColorRange.YELLOW);
         camera.scanBlobs(ColorRange.RED);
         camera.scanBlobs(ColorRange.BLUE);
+
+        gamepad = new GamepadEx(gamepad1);
     }
 
     @Override
     public void run() {
         super.run();
+        if (gamepad1.a) {
+            List<Point> yellow = camera.scanBlobs(ColorRange.YELLOW);
+            if (!yellow.isEmpty()) {
+                Point center = yellow.get(0);
+                elementFocus(center.x);
+            } else {
+                follower.manual(0, 0, 0);
+            }
+        }
+
+
+        telemeter();
+        follower.update();
+    }
+
+    @Override
+    public void end() {
+        if (follower != null) {
+            follower.manual(0, 0, 0);
+            follower.update();
+        }
+        super.stop();
+        if (camera != null) camera.close();
+    }
+
+    public void telemeter() {
         multipleTelemetry.addData("Camera", camera.getCameraState());
         multipleTelemetry.addData("FPS", "%.1f", camera.getFps());
 
@@ -50,10 +87,26 @@ public class CameraTest extends ActionOpMode {
         multipleTelemetry.update();
     }
 
-    @Override
-    public void end() {
-        super.stop();
-        if (camera != null) camera.close();
+    public void elementFocus(double posX) {
+        double center = 640 / 2.0; // width resolution / 2
+        double offset = center - posX;
+        double degrees = Math.abs(offset) > 5
+            ? Range.clip(offset * 0.02, -5, 5) : 0;
+        Pose current = follower.pose();
+        follower.hold(new Pose(
+            current.x(),
+            current.y(),
+            current.heading() + Math.toRadians(degrees)
+        ));
     }
+
+//    this func is bad
+//    if (me.willPower > 0) {
+//        make function find out the distance via size off blob;
+//        do trigo to find angle via distance and offset;
+//        move that angle;
+//
+//        currently it just moves gradually until it locks onto the pollen :(
+//     }
 
 }
