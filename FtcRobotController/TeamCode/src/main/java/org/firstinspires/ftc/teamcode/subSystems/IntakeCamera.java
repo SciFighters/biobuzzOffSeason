a20e29ce5ceba14d8b96764c9385c2e7c1dd5cdb
Ori Supino <ori@supino.org>
2026-09-26 20:54:45 +0300

    camera now sees color, very concerning

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/IntakeCamera.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subSystems/IntakeCamera.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.subSystems;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.Size;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -24,7 +27,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
+@Config
 public class IntakeCamera implements AutoCloseable {
+
+    public static double minBlobSize = 2000;
 
     private final VisionPortal portal;
     private final Map<ColorRange, ColorBlobLocatorProcessor> blobProcessors = new HashMap<>();
@@ -36,7 +42,7 @@ public class IntakeCamera implements AutoCloseable {
         portal = new VisionPortal.Builder()
                 .setCamera(hm.get(WebcamName.class, "cam"))
                 .setCameraResolution(new Size(640, 480)) // 480p
-                .enableLiveView(false)
+                .enableLiveView(true)
                 .addProcessor(new VisionProcessor() {
                     @Override
                     public void init(int width, int height, CameraCalibration cameraCalibration) {
@@ -48,13 +54,20 @@ public class IntakeCamera implements AutoCloseable {
 
                     @Override
                     public Object processFrame(Mat frame, long captureTimeNanos) {
+                        Map<ColorBlobLocatorProcessor, Object> drawContexts = new HashMap<>();
+                        ColorBlobLocatorProcessor.BlobFilter areaFilter =
+                                new ColorBlobLocatorProcessor.BlobFilter(
+                                        ColorBlobLocatorProcessor.BlobCriteria.BY_CONTOUR_AREA,
+                                        minBlobSize, Double.MAX_VALUE);
                         synchronized (IntakeCamera.this) {
                             for (Map.Entry<ColorRange, ColorBlobLocatorProcessor> entry : blobProcessors.entrySet()) {
                                 ColorBlobLocatorProcessor processor = entry.getValue();
                                 if (initializedBlobProcessors.add(processor)) {
                                     processor.init(frame.cols(), frame.rows(), calibration);
                                 }
-                                processor.processFrame(frame, captureTimeNanos);
+                                processor.removeAllFilters();
+                                processor.addFilter(areaFilter);
+                                drawContexts.put(processor, processor.processFrame(frame, captureTimeNanos));
                                 List<Point> centers = new ArrayList<>();
                                 for (ColorBlobLocatorProcessor.Blob blob : processor.getBlobs()) {
                                     Point center = blob.getBoxFit().center;
@@ -63,24 +76,36 @@ public class IntakeCamera implements AutoCloseable {
                                 blobCenters.put(entry.getKey(), centers);
                             }
                         }
-                        return null;
+                        return drawContexts;
                     }
 
                     @Override
+                    @SuppressWarnings("unchecked")
                     public void onDrawFrame(Canvas canvas, int width, int height,
                                             float scaleBmpPxToCanvasPx, float scaleCanvasDensity,
                                             Object userContext) {
+                        Map<ColorBlobLocatorProcessor, Object> drawContexts =
+                                (Map<ColorBlobLocatorProcessor, Object>) userContext;
+                        synchronized (IntakeCamera.this) {
+                            for (Map.Entry<ColorBlobLocatorProcessor, Object> entry : drawContexts.entrySet()) {
+                                entry.getKey().onDrawFrame(canvas, width, height,
+                                        scaleBmpPxToCanvasPx, scaleCanvasDensity, entry.getValue());
+                            }
+                        }
                     }
                 })
                 .addProcessors(processors)
                 .build();
+        FtcDashboard.getInstance().startCameraStream(portal, 10);
     }
 
     public void startStreaming() {
         portal.resumeStreaming();
+        FtcDashboard.getInstance().startCameraStream(portal, 10);
     }
 
     public void stopStreaming() {
+        FtcDashboard.getInstance().stopCameraStream();
         portal.stopStreaming();
     }
 
@@ -98,6 +123,7 @@ public class IntakeCamera implements AutoCloseable {
 
     @Override
     public void close() {
+        FtcDashboard.getInstance().stopCameraStream();
         portal.close();
     }
 
@@ -106,11 +132,16 @@ public class IntakeCamera implements AutoCloseable {
         Objects.requireNonNull(color, "color");
         ColorBlobLocatorProcessor processor = blobProcessors.get(color);
         if (processor == null) {
+            int overlayColor = color == ColorRange.RED ? Color.RED
+                    : color == ColorRange.BLUE ? Color.BLUE
+                    : color == ColorRange.YELLOW ? Color.YELLOW : Color.WHITE;
             processor = new ColorBlobLocatorProcessor.Builder()
                     .setTargetColorRange(color)
                     .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
                     .setRoi(ImageRegion.entireFrame())
-                    .setDrawContours(false)
+                    .setDrawContours(true)
+                    .setContourColor(overlayColor)
+                    .setBoxFitColor(overlayColor)
                     .build();
             blobProcessors.put(color, processor);
         }
